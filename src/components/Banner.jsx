85f2aeb507f8d587600b4991aae82d28472dc3a4
Pranusha <narsicloud@gmail.com>
2024-08-31 21:44:09 -0500

    OrbitReport

---
+++ src/components/Banner.jsx
@@ -0,0 +1,6 @@
+export default function Banner(){
+    return (<div>
+        <header>Orbit Report</header>
+        <p>Click on the buttons to see the satellites in that orbit type</p>
+        </div>);
+}
